be1aeb71226af7de1be4a60ca3c8b8566a1d5644
Richard Ramos <info@richardramos.me>
2017-08-23 13:28:54 -0400

    Initial version

---
+++ contracts/Splitter.sol
@@ -0,0 +1,76 @@
+pragma solidity ^0.4.6;
+
+contract Splitter {
+    
+    address owner;
+    
+    mapping(address => uint) balance;
+    
+    modifier restricted() {
+        if (msg.sender == owner) _;
+    }
+    
+    
+    function Splitter() {
+        owner = msg.sender;
+    }
+	
+    event LogSplit(address sender, address rec1, address rec2, uint amount);
+    event LogWithdraw(address to, bool success);
+    event LogKillSwitch();
+    
+    function split(address rec1, address rec2) 
+        public 
+        payable 
+        returns(bool) {
+        
+        require(msg.value > 0);
+        
+        balance[rec1] += msg.value / 2;
+        balance[rec2] += msg.value / 2;
+        
+        if(msg.value % 2 == 1){
+            balance[msg.sender] += 1;
+        }
+        
+        LogSplit(msg.sender, rec1, rec2, msg.value);
+		
+        return true;
+    }
+    
+    
+    function getBalance(address rec)
+        public
+        constant
+        returns(uint) {
+        return balance[rec];
+    }
+    
+    
+    function withdraw()
+        public 
+        returns(bool) {
+
+        uint amount = balance[msg.sender];
+
+        require(amount > 0);
+        
+        if(msg.sender.send(amount)){
+            balance[msg.sender] = 0;
+            LogWithdraw(msg.sender, true);
+            return true;
+        } else {
+            LogWithdraw(msg.sender, false);
+            return false;
+        }
+    }
+    
+    
+    function killSwitch() 
+        public
+        restricted {
+        suicide(owner);
+        LogKillSwitch();
+    }
+    
+}
