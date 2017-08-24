f22f10cb18374cff35abcdd29351de5f95b6bd9f
Richard Ramos <info@richardramos.me>
2017-08-23 21:40:58 -0400

    Added Remitance.sol logic

--- contracts/Remitance.sol
+++ contracts/Remitance.sol
@@ -3,58 +3,158 @@ pragma solidity ^0.4.6;
 contract Remitance {
     
     address owner;
+    
     uint fee;
     
-    struct TransactionStruct {
-    	// TODO
+    uint feeBalance;
+    
+    struct TransactionStruct { 
+        address origin; // Alice
+        address destination; // Bob
+        address thirdParty;
+        uint amount;
+        uint deadlineBlock;
+        bool exists;
+        bool hasDeadline;
     }
     
+    mapping(bytes32 => TransactionStruct) remitanceBook;
+
+    event LogSetFees(uint newFee);
+    event LogWithdrawFees(uint balance, bool success);
+    event LogSend(address origin, address destination, address thirdParty, uint deadlineBlock, bytes32 keyHash);
+    event LogCollect(bytes32 keyHash, bool success);
+    event LogRefund(bytes32 keyHash, bool success);
+    event LogKillSwitch();
+
     modifier restricted() {
         if (msg.sender == owner) _;
     }
     
-    
     function Remitance() {
         owner = msg.sender;
-        fee = 25000;
+        fee = 200000;
+        feeBalance = 0;
     }
 
     function setFee(uint newFee)
-    	public
-    	returns(bool) {
-    	fee = newFee;
+        public
+        restricted 
+        returns(bool) {
+        fee = newFee;
+        LogSetFees(newFee);
+        return true;
+    }
+    
+    function checkAccumulatedFees()
+        public
+        constant
+        restricted
+        returns(uint){
+            return feeBalance;
+    }
+    
+    function withdrawAccumulatedFees()
+        public
+        restricted 
+        returns(bool) {
+        
+        if(owner.send(feeBalance)){
+            LogWithdrawFees(feeBalance, true);
+            feeBalance = 0;
+            return true;
+        } else {
+            LogWithdrawFees(feeBalance, false);
+            return false;
+        }
+    }
+    
+    function send(address destination, address thirdParty, uint deadlineBlock, bytes32 pwd1, bytes32 pwd2)
+        public 
+        payable 
+        returns(bool) {
+            
+            if(int(msg.value) - int(fee) < 0) revert();
+            
+            bytes32 keyHash = keccak256(pwd1, pwd2);
+
+            require(!remitanceBook[keyHash].exists);
+
+            bool hasDeadline = deadlineBlock > 0;
+            
+            remitanceBook[keyHash] = TransactionStruct(msg.sender, destination, thirdParty, msg.value - fee, block.number + deadlineBlock, true, hasDeadline);
+            
+            feeBalance += fee;
+            
+            LogSend(msg.sender, destination, thirdParty, deadlineBlock, keyHash);
+            
+            return true;
+    }
+    
+    function checkRemitanceBalance(bytes32 pwd1, bytes32 pwd2)
+        constant
+        returns(uint){
+            bytes32 keyHash = keccak256(pwd1, pwd2);
+            
+            require(remitanceBook[keyHash].exists && 
+                   (remitanceBook[keyHash].thirdParty == msg.sender || msg.sender == owner)
+                );
+                
+            return remitanceBook[keyHash].amount;
+    }
+    
+    function collect(bytes32 pwd1, bytes32 pwd2)
+        public 
+        returns(bool){
+            
+        bytes32 keyHash = keccak256(pwd1, pwd2);
+        
+        require(remitanceBook[keyHash].exists &&  
+                remitanceBook[keyHash].thirdParty == msg.sender && 
+                remitanceBook[keyHash].amount > 0 
+                );
+        
+         if(remitanceBook[keyHash].destination.send(remitanceBook[keyHash].amount)){
+            remitanceBook[keyHash].exists = false;
+            LogCollect(keyHash, true);
+            return true;
+        } else {
+            LogCollect(keyHash, false);
+            return false;
+        }
     }
 
-
-    function send(address rec, uint deadline, bytes32 pHash1, bytes32 pHash2)
-    	public 
-    	payable 
-    	returns(bool) {
-    		// TODO save exchangeowner (carol)
-
+    function refund(bytes32 pwd1, bytes32 pwd2)
+        returns(bool){
+            
+        bytes32 keyHash = keccak256(pwd1, pwd2);
+            
+        require(remitanceBook[keyHash].exists && 
+                remitanceBook[keyHash].origin == msg.sender && 
+                remitanceBook[keyHash].amount > 0
+                );
+        
+        if(remitanceBook[keyHash].hasDeadline){
+            require(block.number > remitanceBook[keyHash].deadlineBlock);
+        }
+        
+        if(msg.sender.send(remitanceBook[keyHash].amount)){
+            remitanceBook[keyHash].exists = false;
+            LogRefund(keyHash, true);
+            return true;
+        } else {
+            LogRefund(keyHash, false);
+            return false;
+        }
     }
-	
-	function collect(bytes32 pHash1, bytes32 pHash2)
-		public 
-		returns(bool){
-			// TODO only exchangeowner can call withdraw abd will send to rec
-	}
-
-	function refund()
-		returns(bool){
-
-		}
     
     
     function killSwitch() 
         public
         restricted {
-
-//  todo return funds?
-
-
-        suicide(owner);
         LogKillSwitch();
+        suicide(owner);
     }
     
 }
+
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var Splitter = artifacts.require("./Splitter.sol");
+var Remitance = artifacts.require("./Remitance.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Splitter);
+  deployer.deploy(Remitance);
 };
